84565c8ae0d49fdc3a02edaf444e4f40707e9096
Vapour101 <vapour101@gmail.com>
2022-05-11 10:55:47 +0200

    Use Firacode with ligatures

--- init.el
+++ init.el
@@ -7,12 +7,7 @@
  '(package-selected-packages
    '(moe-theme projectile haskell-mode arduino-mode csharp-mode rjsx-mode bison-mode gitignore-mode gitconfig-mode gitattributes-mode rainbow-delimiters paredit lsp-mode helpful org-bullets org-plus-contrib undo-tree company magit deadgrep move-text diff-hl minions moody rustic yasnippet-snippets yasnippet flycheck counsel swiper ivy auto-compile use-package))
  '(safe-local-variable-values '((create-lockfiles))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 
 ;; Ensure we have all the package archives we need
 (require 'package)
@@ -26,5 +21,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(load-file "~/.emacs.d/ligature/ligature.el")
+
 ;; Load up our config
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#fdfdf6" :foreground "#5f5f5f" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "CTDB" :family "Fira Code")))))
